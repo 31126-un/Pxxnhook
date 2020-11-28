192da7e6f9ca9c53c3e0d6aa3199d7833d15e36a
beichen <beichenhpy@outlook.com>
2020-11-28 15:53:57 +0800

    '实现baidu.cm网站的去除登录限制'

--- bg.js
+++ bg.js
@@ -1,7 +1,6 @@
 chrome.webRequest.onBeforeRequest.addListener(
   function (details) {
-    console.log("测试")
-    return { redirectUrl: chrome.extension.getURL("hook.js") }; //hook.js是你要替换的js脚本
+    return { redirectUrl: "https://cdn.jsdelivr.net/gh/beichenhpy/Pxxnhook@1.0/hook.js" }; //hook.js是你要替换的js脚本
   },
   {
     urls: ["*://baidu.cm/static/msvodx/js/video.js"],  //你要拦截的url地址
